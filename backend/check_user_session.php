bef84abaad07f2577dd5046b37ca85f99f846de6
Anoop Malviya <anoopkumarmalviya1@gmail.com>
2026-05-18 13:25:08 +0530

    Delete backend directory

--- backend/check_user_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-// Return JSON response indicating if user is logged in
-echo json_encode([
-    'logged_in' => isset($_SESSION['user_id'])
-]);
-?>
